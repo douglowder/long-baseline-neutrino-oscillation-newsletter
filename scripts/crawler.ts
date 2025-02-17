5ef62bbf9710274af99632948991c51bfa289de1
Douglas Lowder <douglowder@mac.com>
2025-02-16 18:15:48 -0800

    Better sanitization of crawled pages

--- scripts/crawler.ts
+++ scripts/crawler.ts
@@ -15,9 +15,37 @@ const timeoutAsync = (ms: number) =>
 // const urlMap = new Map<string, string>();
 
 const rootUrl = 'http://www.hep.anl.gov/ndk/longbnews';
+const rootUrlRegEx = new RegExp(`${rootUrl}/`, 'g');
 
 const filenameForUrl = (url: string) =>
   url === rootUrl ? 'index.html' : path.basename(url);
+
+const sanitizeHtml = (html: string) => {
+  // Trim lines
+  const trimmedHtml = html
+    .split('\n')
+    .map((l) => l.trim().replace(rootUrlRegEx, ''))
+    .join('\n');
+  // Remove script tags
+  const $ = cheerio.load(trimmedHtml);
+  $('script').each((index, element) => {
+    $(element).remove();
+  });
+  // Remove javascript anchors
+  $('a').each((index, element) => {
+    const childUrl = $(element).attr('href');
+    if (childUrl?.startsWith('javascript:')) {
+      $(element).remove();
+    }
+    if (childUrl?.startsWith('/cdn-cgi')) {
+      $(element).remove();
+    }
+  });
+  // Remove "mail to" tag
+  const editedHtml: string = $.html().replace('<br>mail to-', '');
+  // Return sanitized HTML
+  return editedHtml;
+};
 
 const crawlAsync = async (url: string) => {
   console.log(`Crawling URL = ${url}`);
@@ -28,7 +56,7 @@ const crawlAsync = async (url: string) => {
   // urlMap.set(url, html);
   await fs.writeFile(
     path.join('.', 'site', filenameForUrl(url)),
-    html.replace(new RegExp(`${rootUrl}/`, 'g'), ''),
+    sanitizeHtml(html),
     {
       encoding: 'utf-8',
     },
